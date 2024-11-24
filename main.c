99dbd483107e73faf27ff5e69000a6648acece7c
Castellani21 <eduardo.castellani2137@gmail.com>
2024-11-24 00:39:40 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,38 @@
- #include <stdio.h>
- 
- void VerificarParImpar(int num); // assinatura da funçao 
- 
-int main(){
-        int numero;
+#include <stdio.h>
+
+int main(void){
+    
+    struct ficha_de_aluno {
+        char nome[50];
+        char disciplina[30];
+        double nota_prova1;
+        double nota_prova2;
+    };
+    
+    struct ficha_de_aluno aluno;
+    
+    printf("---------------- Cadastro de aluno---------------- \n \n \n");
+    
+    printf("nome do aluno ............");
+    fgets(aluno.nome, 50, stdin);
+    
+    printf("Disciplina...........");
+        fgets(aluno.disciplina, 30, stdin);
         
-        printf("digite um numero");
-            scanf("%d", &numero);
+        printf("informe a primeira nota ...:");
+            scanf("%lf", &aluno.nota_prova1);
             
-            VerificarParImpar(numero); // chama a funçao 
-           
+            printf("informe a segunta nota....:");
+                scanf("%lf", &aluno.nota_prova2);
+                
+                printf("\n \n --------- lendo os dados da Struct --------------");
+                printf("Nome-------------------- %s",aluno.nome);
+                printf("Disciplina-----------------%s",aluno.disciplina);
+                printf("nota da prova 1----------------- %.2f", aluno.nota_prova1);
+                printf("nota da prova 2----------------- %.2f", aluno.nota_prova2);
+
+                
+                
+    
     return 0;
 }
-
-void VerificarParImpar(int num){    //funçao 
-            for(int i =0; i < num; i++){
-                
-    if(i % 2 == 0){
-        printf("%d é par \n", i);
-    }
-        else{
-            printf("%d é impar\n", i);
-        }
-
-            } 
-  
-}      
-
- 
- 
